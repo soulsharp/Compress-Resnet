e0b8826389425a3de852804589c674611bfafb74
Soulsharp <138900246+soulsharp@users.noreply.github.com>
2025-08-29 12:49:18 +0530

    feat: add dataloader logic to pl_module

--- model/resnet_pl.py
+++ model/resnet_pl.py
@@ -1,31 +1,34 @@
 import torch
 from torch import nn
-from torchvision.models import resnet50
 import pytorch_lightning as pl
 from torchmetrics import Accuracy
 from typing import Any
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler
 
+from model.resnet import resnet50
 from model.scheduler import WarmupCosineLR
+from data.load_data import prepare_dataloader
+from benchmarks.benchmark import run_benchmark
+
 
 class Resnet50Module(pl.LightningModule):
-    def __init__(self, num_classes, cfg, pretrained_path=None):
+    def __init__(self, cfg, num_classes=10, pretrained_path="model/weights"):
         super().__init__()
         assert cfg is not None, "Config cannot be None"
         self.cfg = cfg
         self.criterion = torch.nn.CrossEntropyLoss()
         self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
 
-        self.model = resnet50(weights=None)        
-        num_features = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_features, num_classes)
-        
-        if pretrained_path:
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            model_state_dict = torch.load(pretrained_path, map_location=device)
-            self.model.load_state_dict(model_state_dict)
-    
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.model = resnet50(
+            pretrained=True, pretrained_dir=pretrained_path, device=device
+        )
+        if num_classes != 10:
+            num_features = self.model.fc.in_features
+            self.model.fc = nn.Linear(num_features, num_classes)
+
     def forward(self, batch):
         images, labels = batch
         predictions = self.model(images)
@@ -44,9 +47,21 @@ class Resnet50Module(pl.LightningModule):
         self.log("loss/val", loss)
         self.log("acc/val", accuracy)
 
+    def train_dataloader(self) -> torch.utils.data.DataLoader:
+        if not hasattr(self, "_train_loader"):
+            self._train_loader = prepare_dataloader(True)
+        return self._train_loader
+
+    def val_dataloader(self) -> torch.utils.data.DataLoader:
+        if not hasattr(self, "_val_loader"):
+            self._val_loader = prepare_dataloader(True)
+        return self._val_loader
+
+    def test_dataloader(self) -> torch.utils.data.DataLoader:
+        return self.val_dataloader()
+
     def test_step(self, batch):
-        _, accuracy = self.forward(batch)
-        self.log("acc/test", accuracy)
+        return None
 
     def configure_optimizers(self) -> tuple[list[Optimizer], list[dict[str, Any]]]:
         optimizer = torch.optim.SGD(
@@ -66,20 +81,11 @@ class Resnet50Module(pl.LightningModule):
         }
         return [optimizer], [scheduler]
 
-def load_resnet():
-    import os
-    model = resnet50(weights=None)
-    pretrained_path = os.path.join("E:/ResNet-Compress/retrain/pretrained_wt_resnet50", "resnet50.pt")
-    print(list(torch.load(pretrained_path).keys())[:10])
-
-    # traced = symbolic_trace(model)
-
-    # for node in traced.graph.nodes:
-    #     print(f"{node.op}: {node.name} -> {node.target}")
-
-    # traced.fc = nn.Linear()
-
-    return model
-
-if __name__ == "__main__":
-    resnet_model = load_resnet()
+    def on_test_start(self):
+        k = self.cfg["k"]
+        acc, avg_time = run_benchmark(self.model, self._val_loader, k)
+        self.log(f"benchmark_top{k}_acc", acc, prog_bar=True)
+        self.log("benchmark_avg_time_per_sample", avg_time)
+        print(
+            f"Top-{k} Benchmark Accuracy: {acc:.4f}, Avg time/sample: {avg_time:.6f}s"
+        )
